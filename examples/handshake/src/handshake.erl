711a5b92c8b16424faa35526514aaf43d7da7f05
Olav Frengstad <olav@fwt.no>
2012-07-22 21:56:53 +0200

    Correct minor typo in log message

--- examples/handshake/src/handshake.erl
+++ examples/handshake/src/handshake.erl
@@ -14,7 +14,7 @@ start() -> application:start(handshake).
 start(_StartType, _StartArgs) ->
 	Args = application:get_all_env(),
 	{_, Port} = lists:keyfind(port, 1, Args),
-	error_logger:info_msg("Starting echo server: 0.0.0.0:~p...~n", [Port]),
+	error_logger:info_msg("Starting handshake server: 0.0.0.0:~p...~n", [Port]),
 	linedancer_sup:start_link(Args).
 
 stop(_State) ->
